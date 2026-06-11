7b5454a1c8fabf3f7d83a6d6df25781970f20c5f
piepenie <piepenie@daum.net>
2026-06-11 21:13:25 +0900

    0611

--- JS/day1.js
+++ JS/day1.js
@@ -14,21 +14,21 @@
 // } // 1개 만 나와야하거든?
 // console.log(b); // 이유는 콘솔을 중괄호 안으로 넣어서 실행했기 때문에 두 번 실행되버린 거임
 
-// function printStars(maxLine){
-//   for (let i = 1; i<= maxLine; i++){
-//     let star = "*"; // 별이 왜 하나 더 나와?
-//     for (let j = 1; j<=i; j++){
-//       star += "*";
-//     }
-//     console.log(star)
-//   }
-// }
-// printStars(4); // 아 별 하나를 변수로 지정해놔서;;
-
-function getTypeMessage(Value){  
-  if(){
-  
+function printStars(maxLine){
+  for (let i = 1; i<= maxLine; i++){
+    let star = "*"; // 별이 왜 하나 더 나와?
+    for (let j = 1; j<=i; j++){
+      star += "*";
+    }
+    console.log(star)
   }
-  console.log('${}는 문자열 입니다')
 }
-getTypeMessage("나는")
+printStars(4); // 아 별 하나를 변수로 지정해놔서;;
+
+// function getTypeMessage(Value){  
+//   if(){
+  
+//   }
+//   console.log('${}는 문자열 입니다')
+// }
+// getTypeMessage("나는")
---
+++ JS/day4.js
@@ -0,0 +1,149 @@
+/////////// 1
+
+// function makeStars(n){
+//   let star ="";
+
+//   for(i=0; i<n;i++){
+//     star += "*";
+//   }
+//   console.log(star)
+// }
+// makeStars(5)
+
+
+/////////// 2
+
+// function makeNumberLine(n){
+//   let number = "";
+
+//   for(i=1; i<=n; i++){
+//     number += i + " ";
+//   }
+//   console.log(number)
+// }
+// makeNumberLine(5)
+
+/////////// 3
+
+// function makeVerticaStars(n){
+//   let star = "";
+//   for(i = 1; i<=n; i++){
+//     for(j=1; j<=i; j++){
+//       star = "*";
+//     }
+//     console.log(star)
+//   }
+// }
+// makeVerticaStars(3)
+
+/////////// 4
+
+// function makeSquare(n){
+//   for(i=1; i<=n; i++){
+//     let star = "";
+//     for(j=1; j<=n; j++){
+//       star += "*"
+//     }
+//     console.log(star)
+//   }
+// }
+// makeSquare(3)
+
+/////////// 5
+
+// function makeRectangle(width, heigth){
+//     for(i=1; i<=heigth; i++){
+//       let star = "";
+//       for(j=1; j<=width; j++){
+//         star += "*"
+//     }
+//     console.log(star)
+//   }
+// }
+// makeRectangle(3,4)
+
+/////////// 6
+
+// function makeTriagle(n){
+//   for(i=1; i<=n; i++){
+//     let star = "";
+//     for(j=1; j<=i; j++){
+//       star +="*";
+//     }
+//     console.log(star)
+//   }
+// }
+// makeTriagle(4)
+
+/////////// 7
+
+// function makeReverseTriangle(n){
+//   for(i=n; i>=1; i--){
+//     let star ="";
+//     for(j=1; j<=i; j++){
+//       star +="*";
+//     }
+//     console.log(star)
+//   }
+// }
+// makeReverseTriangle(4)
+
+/////////// 8
+
+// function makeRightTriangle(n){
+//   for(i=1; i<=n; i++) {
+//     let star = "";
+//     let space = "";
+//     for(j=1; j<=i; j++){
+//       star += "*";
+//     }
+//     for(j=1; j<=n-i; j++){
+//       space += " ";
+//     }
+//     console.log(space+star)
+//   }
+// } 
+// makeRightTriangle(4)
+
+/////////// 9
+
+// function makeNumberTriangle(n){
+//   for(i=1; i<=n; i++){
+//     let number = "";
+//     for(j=1; j<=i; j++){
+//       number += j;
+//     }
+//     console.log(number)
+//   }
+// }
+// makeNumberTriangle(4)
+
+/////////// 10 << 어려워 ㅠㅠㅠ 
+
+// function makeBorderSquare(n){
+//   for(i=1; i<=n; i++){
+//     let star = "";
+//     let space = "";
+//     for(j=1; j<=n; j++){
+//       star += "*";
+//     }
+//     console.log(star)
+//   }
+// }
+
+// makeBorderSquare(4)
+
+/////////// 11
+
+// function makeReverseNumberLine(n){
+//   let number = "";
+
+//   for(i=n; i>=1; i--){
+//     number += i + " ";
+//   }
+//   console.log(number)
+// }
+// makeReverseNumberLine(5)
+
+/////////// 12
+
