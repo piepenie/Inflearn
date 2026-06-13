7b4976fb736a77af2d993779f8571a19c97d7fd8
piepenie <piepenie@daum.net>
2026-06-13 23:48:59 +0900

    0613

---
+++ JS/day3.js
@@ -0,0 +1,50 @@
+// console.log('Hello World!');
+// console.log(1);
+// console.log(true);
+// console.log({name: '홍길동', age: 20, married: false});
+// console.log('Hi!', 100, true, [1, 2, 3]);
+
+// let x = 3;
+
+// x += 2;
+// console.log(x);
+
+// x -= 3;
+// console.log(x);
+
+// x *= 12;
+// console.log(x);
+
+// x /= 3;
+// console.log(x);
+
+// x %= 5;
+// console.log(x);
+
+// x **= 4;
+// console.log(x)
+
+// console.log(true, typeof true);
+// console.log(false, typeof false);
+
+// let a = 1 === 2;
+// let b = 'abc' !== 'def'
+// let c = a !== b;
+// let d = typeof a === typeof b === true;
+
+// console.log(a, typeof a);
+// console.log(b, typeof b);
+// console.log(c, typeof c);
+// console.log(d, typeof d);
+
+console.log(
+  true, !true, false, !false
+);
+
+console.log(
+  true, !true, !!true, !!!true
+);
+
+console.log(
+  false, !false, !!false, !!!false
+);
