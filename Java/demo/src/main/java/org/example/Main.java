4cb89abd8a40b4224f3cf04a918545a64e7cd34a
piepenie <piepenie@daum.net>
2026-06-22 13:07:26 +0900

    0622

--- Java/demo/src/main/java/org/example/Main.java
+++ Java/demo/src/main/java/org/example/Main.java
@@ -12,29 +12,133 @@ public class Main {
 //
 //        scanner.close();
 
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("점수를 입력하세요: ");
+//        int score = scanner.nextInt();
+//
+//        String grade;
+//
+//        if (score >= 90) {
+//            grade = "A";
+//        } else if (score >= 80) {
+//            grade = "B";
+//        } else if (score >= 70) {
+//            grade = "C";
+//        } else if (score >= 60) {
+//            grade = "D";
+//        } else {
+//            grade = "F";
+//        }
+//
+//        System.out.println("입력한 점수: " + score);
+//        System.out.println("등급: " + grade);
+//
+//        scanner.close();
 
-        System.out.print("점수를 입력하세요: ");
-        int score = scanner.nextInt();
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("학생 수를 입력하세요: ");
+//        int studentCount = scanner.nextInt();
+//
+//        int total = 0;
+//
+//        for (int i = 1; i <= studentCount; i++) {
+//            System.out.print(i + "번째 학생 점수: ");
+//            int score = scanner.nextInt();
+//
+//            String grade;
+//
+//            if (score >= 90) {
+//                grade = "A";
+//            } else if (score >= 80) {
+//                grade = "B";
+//            } else if (score >= 70) {
+//                grade = "C";
+//            } else if (score >= 60) {
+//                grade = "D";
+//            } else {
+//                grade = "F";
+//            }
+//
+//            System.out.println(i + "번째 학생 등급: " + grade);
+//
+//            total = total + score;
+//        }
+//
+//        System.out.println("전체 점수 합계: " + total);
+//
+//        scanner.close();
 
-        String grade;
+//        Q1. 1~10까지 숫자 중 짝수를 출력합니다.
+//        for (int i = 1; i <= 10; i++) {
+//            if(i%2 == 0){
+//                System.out.println(i);
+//            }
+//        }
 
-        if (score >= 90) {
-            grade = "A";
-        } else if (score >= 80) {
-            grade = "B";
-        } else if (score >= 70) {
-            grade = "C";
-        } else if (score >= 60) {
-            grade = "D";
-        } else {
-            grade = "F";
-        }
+//        Q2. 1~20까지 숫자의 합계를 출력합니다.
+//        int sum = 0;
+//        for (int i = 1; i <= 20; i++) {
+//            sum = sum + i;
+//        }
+//        System.out.println("합계 : " + sum);
 
-        System.out.println("입력한 점수: " + score);
-        System.out.println("등급: " + grade);
+//        Q3.점수 하나를 변수에 담고 등급을 출력합니다. (Scanner 활용)
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("점수를 입력하세요 :");
+//
+//        int score = scanner.nextInt();
+//        String grade;
+//
+//        if (score >= 90) {
+//            grade = "A";
+//        } else if (score >= 80) {
+//            grade = "B";
+//        } else if (score >= 70) {
+//            grade = "C";
+//        } else if (score >= 60) {
+//            grade = "D";
+//        } else {
+//            grade = "F";
+//        }
+//
+//        System.out.println("입력한 점수: " + score);
+//        System.out.println("등급: " + grade);
+//
+//        scanner.close();
 
-        scanner.close();
+//        Q4.
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("이름을 입력하세요 : ");
+//        String name = scanner.nextLine();
+//
+//        System.out.print("점수를 입력하세요 : ");
+//        int score = scanner.nextInt();
+//        String grade;
+//
+//        if (score >= 90) {
+//            grade = "A";
+//        } else if (score >= 80) {
+//            grade = "B";
+//        } else if (score >= 70) {
+//            grade = "C";
+//        } else if (score >= 60) {
+//            grade = "D";
+//        } else {
+//            grade = "F";
+//        }
+//
+//        System.out.println(name + " 학생의 등급은: " + grade);
+//
+//
+//        scanner.close();
+
+//        Q5.`*`을 한 줄씩 늘려서 출력합니다. (별 4개까지)
+//        for (int i = 1; i <= 4; i++) {
+//            System.out.println("*".repeat(i));
+//        }
 
     }
 }
