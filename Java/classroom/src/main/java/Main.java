c3f844b14e8bdafa2c8056ca75ec25e14780a30d
piepenie <piepenie@daum.net>
2026-06-26 18:29:32 +0900

    Main

---
+++ Java/classroom/src/main/java/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("학생 조회 프로그램이 동작중입니다.");
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        Main app = new Main();
+        app.run();
+    }
+
+}
