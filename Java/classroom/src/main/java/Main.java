de7ddaa9e0fc134ee6dd1b96eb9c0f69d622977b
piepenie <piepenie@daum.net>
2026-07-01 22:12:53 +0900

    0701

--- Java/classroom/src/main/java/Main.java
+++ Java/classroom/src/main/java/Main.java
@@ -50,6 +50,7 @@ public class Main {
         Optional<Student> topStudent = classroom.findTopStudent();
         System.out.println(topStudent);
 
+
         scanner.close();
 
     }
---
+++ Java/demo/src/main/java/org/example/Day6.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Day6 {
+    // SpringBoot 넘어가면서 궁금했던거
+    // Class 에는 원래 . 을 붙이지 마라고 했지만,
+    // 간혹 클래스에 점을 붙여서 기능을 바로 불러오는게 있다. 왜?
+    // 분명 클래스에는 점! 을 붙이지마라고 그렇게누누히 강조햇는데 어째서? 
+    // 이 해당에 대해서 설명할 줄 알아야함.
+    
+
+    public static void main(String[] args) {
+
+    }
+}
