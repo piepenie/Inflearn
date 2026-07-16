0cd566bd2f5f16299f95c703b43e064ab33c95bb
piepenie <piepenie@daum.net>
2026-07-16 23:49:04 +0900

    0716

---
+++ Java/JavaPrac/JavaPrac/src/main/java/org/example/javaprac/JavaPracApplication.java
@@ -0,0 +1,13 @@
+package org.example.javaprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaPracApplication.class, args);
+    }
+
+}
