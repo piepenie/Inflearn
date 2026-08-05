548276b5b12b69cb0662bcccbc48db060d811e14
piepenie <piepenie@daum.net>
2026-08-05 21:59:34 +0900

    0805

---
+++ SpringBoot/Docker/docker-prac/src/main/java/inflearn/dockerprac/DockerPracApplication.java
@@ -0,0 +1,13 @@
+package inflearn.dockerprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerPracApplication.class, args);
+    }
+
+}
---
+++ SpringBoot/Docker/docker-prac/src/test/java/inflearn/dockerprac/DockerPracApplicationTests.java
@@ -0,0 +1,13 @@
+package inflearn.dockerprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
